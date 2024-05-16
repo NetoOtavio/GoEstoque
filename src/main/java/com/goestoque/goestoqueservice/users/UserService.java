5cc88da024c2ccd01572166d3d6737409af14adb
Otavio Neto <contatonetootavio@gmail.com>
2024-05-16 12:50:08 -0300

    adding UserService

--- src/main/java/com/goestoque/goestoqueservice/users/UserService.java
+++ src/main/java/com/goestoque/goestoqueservice/users/UserService.java
@@ -1,2 +1,21 @@
-package com.goestoque.goestoqueservice.users;public class UserService {
+package com.goestoque.goestoqueservice.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void create(User user) {
+        repository.save(user);
+    }
+
+    public List<User> readAllUsers() {
+        return repository.findAll();
+    }
 }
