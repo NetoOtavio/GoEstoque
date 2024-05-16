34e01e956655d30427d4bda8a7c5fcc117706852
Otavio Neto <contatonetootavio@gmail.com>
2024-05-16 12:32:30 -0300

    adjustiment in UserRepository

--- src/main/java/com/goestoque/goestoqueservice/users/UserRepository.java
+++ src/main/java/com/goestoque/goestoqueservice/users/UserRepository.java
@@ -1,6 +1,6 @@
 package com.goestoque.goestoqueservice.users;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
