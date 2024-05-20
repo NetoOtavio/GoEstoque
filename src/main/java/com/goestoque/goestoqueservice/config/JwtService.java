f7a1328cbff952f7d7c1a1766cbed20f73d1c0cf
Otavio Neto <contatonetootavio@gmail.com>
2024-05-20 07:03:00 -0300

    updating JWT service|extract claims|extract single claim|extract username

--- src/main/java/com/goestoque/goestoqueservice/config/JwtService.java
+++ src/main/java/com/goestoque/goestoqueservice/config/JwtService.java
@@ -1,10 +1,39 @@
 package com.goestoque.goestoqueservice.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "f520f19c98d4bc436b793da531d7a1ef44b397e86660ff647de27586ed7f466c";
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
